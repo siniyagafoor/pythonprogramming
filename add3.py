da40cae0cb758c1c2fe83efd9a8dee151e11467d
siniyagafoor <145668104+siniyagafoor@users.noreply.github.com>
2023-09-30 15:15:15 +0530

    Create add3.py

---
+++ add3.py
@@ -0,0 +1,6 @@
+print("enter 3 number")
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+largest =max(num1,num2,num3)
+print("the greater number is",largest)
