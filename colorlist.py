fb27c9262c080f96c41f6c64845d088e2432f135
siniyagafoor <145668104+siniyagafoor@users.noreply.github.com>
2023-10-26 16:29:32 +0530

    Add files via upload

---
+++ colorlist.py
@@ -0,0 +1,9 @@
+color_list1={"white","black","red"}
+color_list2={"red","green"}
+print("original ser elements")
+print(color_list1)
+print(color_list1)
+print("\ndifference of color list1 & color_list2")
+print(color_list1-color_list2)
+print("\ndifference of color list2 & color_list1")
+print(color_list2-color_list1)
