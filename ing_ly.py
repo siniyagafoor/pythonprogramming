c33e9e4e7e46577b6b0dba94d53d5631245a01cb
siniyagafoor <145668104+siniyagafoor@users.noreply.github.com>
2023-11-09 16:18:46 +0530

    Add files via upload

---
+++ ing_ly.py
@@ -0,0 +1,10 @@
+def ing(str1):
+    length=len(str1)
+    if length>2:
+        if str1[-3:]=='ing':
+            str1+='ly'
+        else:
+            str1+='ing'
+        return str1
+n=input("enter the word")
+print(ing(n))
